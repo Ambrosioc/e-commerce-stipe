8ef32e77062d6a0d940d7a2c1a134354b211b868
Ambrosie CAZIMIRA <a.cazimira@gmail.com>
2023-09-21 19:37:55 +0200

    ad products and cart

---
+++ app.js
@@ -0,0 +1,12 @@
+// Cart Open Close
+const cartIcon = document.querySelector("#cart-icon");
+const cart = document.querySelector(".cart");
+const closeCart = document.querySelector("#close-cart");
+// Open Cart
+cartIcon.onclick = () => {
+	cart.classList.add("active");
+};
+// Close Cart
+closeCart.onclick = () => {
+	cart.classList.remove("active");
+};
